b94986a20b26f18af1e68035da174f8c5111eb1c
Alejandro Navarro <alenvbn@gmail.com>
2024-12-20 13:33:58 -0600

    End of section 3

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -9,5 +9,6 @@ public class Product : BaseEntity
     public decimal Price { get; set; }
     public required string PictureUrl { get; set; }
     public required string Type { get; set; }
+    public required string Brand { get; set; }
     public int QuantityInStock { get; set; }
 }
