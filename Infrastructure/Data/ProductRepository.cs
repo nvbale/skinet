67a23613501e8626f9c1c284ff4e9c94f271f479
Alejandro Navarro <alenvbn@gmail.com>
2024-12-22 15:56:18 -0600

    End of section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,3 @@
-using System;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -19,7 +18,9 @@ public class ProductRepository(StoreContext context) : IProductRepository
 
     public async Task<IReadOnlyList<string>> GetBrandsAsync()
     {
-        return await context.Products.Select(x => x.Brand).Distinct().ToListAsync();
+        return await context.Products.Select(x => x.Brand)
+        .Distinct()
+        .ToListAsync();
     }
 
     public async Task<Product?> GetProductByIdAsync(int id)
@@ -48,7 +49,9 @@ public class ProductRepository(StoreContext context) : IProductRepository
 
     public async Task<IReadOnlyList<string>> GetTypesAsync()
     {
-        return await context.Products.Select(x => x.Type).Distinct().ToListAsync();
+        return await context.Products.Select(x => x.Type)
+        .Distinct()
+        .ToListAsync();
     }
 
     public bool ProductExists(int id)
