78cc56527f8a9b8f85fbdb4776f065d00f9a6da2
nvbale <nvbalek@gmail.com>
2025-02-19 10:39:52 -0600

    End of section 13

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,9 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection
+} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
@@ -6,8 +11,28 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {errorInterceptor} from './core/interceptors/error.interceptor';
 import {loadingInterceptor} from './core/interceptors/loading.interceptor';
+import {InitService} from './core/services/init.service';
+import {lastValueFrom} from 'rxjs';
+
+function initializeApp(initService: InitService) {
+  lastValueFrom(initService.init()).finally(() => {
+    const splash = document.getElementById('initial-splash')
+    if (splash) {
+      splash.remove()
+    }
+  })
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([
+      errorInterceptor,
+      loadingInterceptor,
+    ])),
+    provideAppInitializer(() => initializeApp(inject(InitService))),
+  ],
+
 };
